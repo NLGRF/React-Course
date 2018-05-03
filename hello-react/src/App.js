5e9c41e0e2b5e224fca65bd09765597fc993b5ac
NLGRF <nongrace6654@gmail.com>
2018-05-04 03:27:10 +0700

    React-Router

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -4,43 +4,14 @@ import Content from './Content';
 import Footer from './Footer';
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state={
-      data:[],
-      message:"UnLike",
-      type:""
-    };
-    this.changeMessage=this.changeMessage.bind(this);
-    this.insertData=this.insertData.bind(this);
-  }
-
-  changeMessage(){
-      this.setState({message:"Like"});
-  }
-  insertData(){
-    var item="React";
-    var myArray=this.state.data;
-    myArray.push(item);
-    this.setState({data:myArray});
-  }
-  onChange(event){
-    this.setState({type:event.target.value})
-  }
     render() {
-
       return (
         <div>
-          <Header />
-          <Content title="Course React" name="Non Robot" price="Free"/>
-          <Footer />
-          <h1>{this.state.message}</h1>
-          <button onClick={this.changeMessage}>{this.state.message}</button><br/>
-          <h1>{this.state.data}</h1>
-          <button onClick={this.insertData}>Insert</button><br/><br/>
-          // <input type="text" onChange={this.onChange.bind(this)}></input>
-          <input type="text" onChange={this.onChange.bind(this)}/>
-          <h1>WelCome : {this.state.type}</h1>
+            <h1>Web Robot</h1>
+            <h1><a href="/">Home</a></h1>
+            <h1><a href="/header">Header</a></h1>
+            <h1><a href="/content">Content</a></h1>
+            <h1><a href="/footer">Footer</a></h1>
         </div>
       );
     }
--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {Router,Route,Link,browserHistory} from 'react-router';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+ <Router history={browserHistory}>
+   <Route path="/" component={App}/>
+   <Route path="/header" component={Header}/>
+   <Route path="/content" component={Content}/>
+   <Route path="/footer" component={Footer}/>
+ </Router>,document.getElementById('root')
+);
 registerServiceWorker();
