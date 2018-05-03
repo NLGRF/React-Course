2a13240deba571ce3621c9ae490fa6807a2b6d8d
NLGRF <nongrace6654@gmail.com>
2018-05-04 01:16:50 +0700

    React-Style & Expression JSX

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 
 class App extends Component {
     render() {
+      var mystyle={
+        fontSize:100,
+        color:'#FF0000'
+      }
       return (
         <div>
-          <h1>React Course</h1>
+          <h1 style={mystyle}>React Course</h1>
+          <h2>{50*50}</h2>
         </div>
       );
     }
