f246941b24f8cac0bc8d268e4a710521a4405d99
NLGRF <nongrace6654@gmail.com>
2018-05-05 05:05:08 +0700

    Redux-Store Reducer & Dispatch

--- hello-redux/src/index.js
+++ hello-redux/src/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+//
+// ReactDOM.render(<App />, document.getElementById('root'));
+import {createStore} from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const reducer=(state,action)=>{
+  switch (action.type) {
+    case "ADD":
+          state+=action.payload;
+      break;
+    case "SUBTRACT":
+          state-=action.payload;
+      break;
+    default:
+
+  }
+  return state;
+}
+const store=createStore(reducer,15000);
+store.subscribe(()=>{
+  console.log("Update Store",store.getState());
+})
+store.dispatch({
+  type:"ADD",
+  payload:500
+});
+store.dispatch({
+  type:"SUBTRACT",
+  payload:10000
+});
